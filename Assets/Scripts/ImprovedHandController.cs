e37dc3d91e50bd1da52af466a67aab66624f7688
KDBenson <k.d.benson@outlook.com>
2021-10-21 15:40:25 -0400

    changes to hands animate, scenes transition w/good lighting

--- Assets/Scripts/ImprovedHandController.cs
+++ Assets/Scripts/ImprovedHandController.cs
@@ -14,24 +14,51 @@ public class ImprovedHandController : MonoBehaviour
     //values only allocate when button pressed, more efficient than checking for input every frame
     private void Awake()
     {
+        _handAnimator = GetHandAnimator();
         controllerActionGrip.action.performed += GripPress;
         controllerActionTrigger.action.performed += TriggerPress;
 
         //in case the animation freezes set returning hand anim and trigger to same
         controllerActionGrip.action.canceled += GripCancel;
 
-        _handAnimator = GetComponent<Animator>();
     }
 
-    private void GripCancel(InputAction.CallbackContext obj) => _handAnimator.SetFloat("Grip", 0);
+    private Animator GetHandAnimator()
+    {
+        Animator _thisAnimator = GetComponent<Animator>();
+        return _thisAnimator;
+    }
+
+    private void GripCancel(InputAction.CallbackContext obj)
+    {
+        if (_handAnimator != null)
+        {
+            _handAnimator.SetFloat("Grip", 0);
+        }        
+
+    }
 
     private void TriggerPress(InputAction.CallbackContext obj)
     {
-        //"Trigger" is linked to the _handAnimator, tells it what blendtree
-        _handAnimator.SetFloat("Trigger", obj.ReadValue<float>());
+        if (_handAnimator != null)
+        {
+            //"Trigger" is linked to the _handAnimator, tells it what blendtree
+            _handAnimator.SetFloat("Trigger", obj.ReadValue<float>());
+
+        }
+
+
     }
 
     //method can be done this way as well
-    private void GripPress(InputAction.CallbackContext obj) => _handAnimator.SetFloat("Grip", obj.ReadValue<float>());
+    private void GripPress(InputAction.CallbackContext obj) {
+        if (_handAnimator != null)
+        {
+
+            _handAnimator.SetFloat("Grip", obj.ReadValue<float>());
+
+        }
+        
+    } 
 
 }
