2fbed3f934fa41ec4ca8e19a3031e3c014acfd84
KDBenson <k.d.benson@outlook.com>
2021-10-22 08:32:55 -0400

    regular commitments of progress

--- Assets/Scripts/ImprovedHandController.cs
+++ Assets/Scripts/ImprovedHandController.cs
@@ -25,6 +25,8 @@ public class ImprovedHandController : MonoBehaviour
 
     private Animator GetHandAnimator()
     {
+        //for some reason it will stop animating properly unless you do it this way
+        //having _handAnimator = GetComponent<Animator>(); up in Awake is useless if more than 1 scene uses hands
         Animator _thisAnimator = GetComponent<Animator>();
         return _thisAnimator;
     }
@@ -35,7 +37,7 @@ public class ImprovedHandController : MonoBehaviour
         {
             _handAnimator.SetFloat("Grip", 0);
         }        
-
+        //when we change scenes what was there is destroyed, so it's going to be crying about nulls without this
     }
 
     private void TriggerPress(InputAction.CallbackContext obj)
@@ -44,21 +46,14 @@ public class ImprovedHandController : MonoBehaviour
         {
             //"Trigger" is linked to the _handAnimator, tells it what blendtree
             _handAnimator.SetFloat("Trigger", obj.ReadValue<float>());
-
         }
-
-
     }
 
-    //method can be done this way as well
     private void GripPress(InputAction.CallbackContext obj) {
         if (_handAnimator != null)
         {
-
             _handAnimator.SetFloat("Grip", obj.ReadValue<float>());
-
         }
-        
     } 
 
 }
